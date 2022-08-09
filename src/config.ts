731c0aa8ed800b3dbe829be61881510c6dbede7f
tobiaskn <tobias.netskar@gmail.com>
2022-08-09 15:47:41 +0200

    Add Astro site

---
+++ src/config.ts
@@ -0,0 +1,19 @@
+export const social = [
+    {
+        icon: 'mdi:instagram',
+        title: 'Instagram',
+        href: 'https://instagram.com/braudtoastbar',
+    },
+    {
+        icon: 'mdi:facebook',
+        title: 'Facebook',
+        href: 'https://www.facebook.com/braudtoastbar',
+    },
+]
+
+export const navigation = [
+    { href: '/', title: 'Hjem', hiddenDesktop: true },
+    { href: '/meny/', title: 'Meny' },
+    { href: '/aapningstider/', title: 'Åpningstider' },
+    { href: '/kontakt/', title: 'Kontakt' },
+]
