b2a9f21447a651a7f1a67f66945f6fcab6f3b8db
tobiaskn <tobias.netskar@gmail.com>
2022-08-09 23:51:56 +0200

    Update Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    ssr: {
-      external: ["svgo"],
+    vite: {
+        ssr: {
+            external: ["svgo"],
+        },
     },
-  },
+    site: 'https://braud.no',
+    base: 'ouhndelof.github.io'
 });
 
